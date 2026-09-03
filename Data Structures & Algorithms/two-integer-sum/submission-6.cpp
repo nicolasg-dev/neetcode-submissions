d818ec76b47b5b72f78bbaad0df9b934a8ac78d9
Nicolas Garcia <ggnicolas28@gmail.com>
2026-09-03 18:26:05 -0300

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        if (nums.size() >= 2){
+            vector<int> saida;
+            for (int i = 0; i < nums.size(); i++){
+                for (int j = 0; j < nums.size(); j++){
+                    if (j != i){
+                        if ((nums[j] + nums[i]) == target){
+                            saida.push_back(i);
+                            saida.push_back(j);
+                            return saida;
+                        }
+                    }
+                }            
+            }
+
+                
+        }
+    }
+};
