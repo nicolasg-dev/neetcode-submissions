b25583371648a527fa1019108098ca0eb052d6de
Nicolas Garcia <ggnicolas28@gmail.com>
2026-09-14 22:57:09 -0300

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> m;
+        for (int num : nums){
+            if (m.count(num)) return true;
+            else {
+                m.insert(num);
+            }
+        }
+        return false;
+    }
+};
